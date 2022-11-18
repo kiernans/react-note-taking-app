8580ac28f71a1cfcdb63a689a7acdd46c91ebd23
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-11-17 23:15:31 -0700

    Create NewNote component

---
+++ src/NewNote.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NoteData, Tag } from './App';
+import { NoteForm } from './NoteForm';
+
+type NewNoteProps = {
+	onSubmit: (data: NoteData) => void;
+	onAddTag: (tag: Tag) => void;
+	availableTags: Tag[];
+};
+
+export const NewNote = ({
+	onSubmit,
+	onAddTag,
+	availableTags,
+}: NewNoteProps) => {
+	return (
+		<>
+			<h1 className='mb-4'>New Note</h1>
+			<NoteForm
+				onSubmit={onSubmit}
+				onAddTag={onAddTag}
+				availableTags={availableTags}
+			/>
+		</>
+	);
+};
